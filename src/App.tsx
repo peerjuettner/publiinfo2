3ef06ddea76207247a830db34ae55ca82fc3008a
Peer Jüttner <peer@hotmail.ch>
2019-06-29 15:55:09 +0200

    add leaflet

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import "typeface-roboto";
 import AppHeader from "./components/Header/Header";
 import PubliInfo from "./publiinfo";
+import "../node_modules/leaflet/dist/leaflet.css";
 
 const App: React.FC = () => {
   return (
--- src/components/BikesPage/BikesPage.tsx
+++ src/components/BikesPage/BikesPage.tsx
@@ -1,17 +1,36 @@
 import * as React from "react";
+import { Map, Marker, TileLayer, ZoomControl } from "react-leaflet";
 
 export interface IBikesPageProps {}
 
-export interface IBikesPageState {}
+export interface IBikesPageState {
+  lat: number;
+  lng: number;
+  zoom: number;
+}
 
 export default class BikesPage extends React.Component<IBikesPageProps, IBikesPageState> {
   constructor(props: IBikesPageProps) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      lat: 46.948,
+      lng: 7.4474,
+      zoom: 13
+    };
   }
 
   public render() {
-    return <div>{"Bikes"}</div>;
+    return (
+      <>
+        <Map center={[this.state.lat, this.state.lng]} zoom={13} zoomControl={false}>
+          <TileLayer
+            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <ZoomControl position="topright" />
+        </Map>
+      </>
+    );
   }
 }
